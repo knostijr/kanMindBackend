e059dae2254f5514f6a273700b49c1661abaf1bc
knostijr <christophkonst@icloud.com>
2026-01-20 17:08:29 +0100

    add URLS core and auth

---
+++ auth_app/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import EmailCheckView, LoginView, RegistrationView
+
+urlpatterns = [
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('email-check/', EmailCheckView.as_view(), name='email-check'),
+]
